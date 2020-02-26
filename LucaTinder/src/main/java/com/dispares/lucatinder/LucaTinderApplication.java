0adeae0b0c68cf2c2c89d7f5ae98d363ce351361
PMGely <pablo.gely.91@gmail.com>
2020-02-26 15:56:27 +0100

    Master inicial con todas las carpetas vacias

---
+++ LucaTinder/src/main/java/com/dispares/lucatinder/LucaTinderApplication.java
@@ -0,0 +1,13 @@
+package com.dispares.lucatinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaTinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaTinderApplication.class, args);
+	}
+
+}
---
+++ LucaTinder/src/test/java/com/dispares/lucatinder/LucaTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dispares.lucatinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaTinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
