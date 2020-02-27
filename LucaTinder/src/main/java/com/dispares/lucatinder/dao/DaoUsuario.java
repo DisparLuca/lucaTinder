ad6d47982d219521b101ec1c9c0bd55386df62fa
Davidubidu <davidfercios@gmail.com>
2020-02-27 14:28:53 +0100

    actuailizado package com.dispares.lucatinder.dao actualizado el import de usuario en interfaz daouUsuario

--- LucaTinder/src/main/java/com/dispares/lucatinder/dao/DaoUsuario.java
+++ LucaTinder/src/main/java/com/dispares/lucatinder/dao/DaoUsuario.java
@@ -3,6 +3,8 @@ package com.dispares.lucatinder.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.dispares.lucatinder.model.Usuario;
+
 @Repository
 public interface DaoUsuario extends JpaRepository<Usuario, Integer>{
 	//comentario developmente funcionar
