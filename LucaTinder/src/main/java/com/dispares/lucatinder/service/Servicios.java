bf64dfb7fb544619d802c9bf74240e72476e6768
Davidubidu <davidfercios@gmail.com>
2020-02-27 15:26:11 +0100

    actualizados imports en com.dispares.lucatinder.service
    actualizados import y javadoc.

--- LucaTinder/src/main/java/com/dispares/lucatinder/service/Servicios.java
+++ LucaTinder/src/main/java/com/dispares/lucatinder/service/Servicios.java
@@ -2,7 +2,14 @@ package com.dispares.lucatinder.service;
 
 import org.springframework.stereotype.Service;
 
+import com.dispares.lucatinder.model.Usuario;
+/**
+ * 
+ * @author David
+ * creación de interfaz de servicios
+ * creación de método salvar usuario
+ */
 @Service
 public interface Servicios {
-
+	public void slavarUsuario(Usuario usuario);
 }
