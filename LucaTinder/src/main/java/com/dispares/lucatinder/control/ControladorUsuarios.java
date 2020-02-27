f30a7ec7e7912bb017ce9112a56482a664a108d8
jbermejoi <jbermejoi@alumnos.unex.es>
2020-02-27 15:08:45 +0100

    crear(falta bbdd) com.dispares.lucatinder.control; ControladorUsuarios resumenUsuarios
    modificacion com.dispares.lucatinder; import
    com.dispares.lucatinder.control.ControladorUsuarios;

--- LucaTinder/src/main/java/com/dispares/lucatinder/control/ControladorUsuarios.java
+++ LucaTinder/src/main/java/com/dispares/lucatinder/control/ControladorUsuarios.java
@@ -1,9 +1,15 @@
 package com.dispares.lucatinder.control;
 
+import javax.validation.Valid;
+
 import org.apache.tomcat.jni.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.dispares.lucatinder.model.Usuario;
 
 /**
  * Controlador de usuarios con anotaciones de Spring
@@ -18,4 +24,21 @@ public class ControladorUsuarios {
 		return "registroUsuario";		
 	}
 	
+	
+	@PostMapping
+	public String resumenUsuarios(
+							@Valid Usuario user,
+							BindingResult result, 
+							ModelMap model) {
+
+		if (result.hasErrors()) {
+			System.out.println("--- Hay algunos errores");
+			return "registroUsuario";
+		}
+
+
+		//aqui falta de hacer la llamada a la base de datos
+		
+		return "resumenUsuarios";
+	}
 }
--- LucaTinder/src/test/java/com/dispares/lucatinder/LucaTinderApplicationTests.java
+++ LucaTinder/src/test/java/com/dispares/lucatinder/LucaTinderApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 
-import com.dispares.lucatinder.control;
+import com.dispares.lucatinder.control.ControladorUsuarios;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
