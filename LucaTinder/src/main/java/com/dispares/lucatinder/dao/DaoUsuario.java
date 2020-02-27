d0b360ae5823afb9d9a90b1a00ec9716e0c615b9
Davidubidu <davidfercios@gmail.com>
2020-02-27 12:39:04 +0100

    creado package com.dispares.lucatinder.service paquete e interfaz servicio
    creados a modo de esqueleto.

--- LucaTinder/src/main/java/com/dispares/lucatinder/dao/DaoUsuario.java
+++ LucaTinder/src/main/java/com/dispares/lucatinder/dao/DaoUsuario.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DaoUsuario extends JpaRepository{
-	
-	public void salvarUsuario();
+public interface DaoUsuario extends JpaRepository<Usuario, Integer>{
+	//
 }
