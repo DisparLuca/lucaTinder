c1101054b70781c45c4f28763a276d1c11a56b7b
alcrac <alcrac1997@gmail.com>
2020-02-27 16:22:29 +0100

    Modificado dao.DaoUsuarioImpl 	Añadido Javadoc 	Modificado List<Usuario>getBusquedaSimple() 		Añadido logger

--- LucaTinder/src/main/java/com/dispares/lucatinder/dao/DaoUsuarioImpl.java
+++ LucaTinder/src/main/java/com/dispares/lucatinder/dao/DaoUsuarioImpl.java
@@ -1,11 +1,13 @@
 package com.dispares.lucatinder.dao;
 
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,14 +16,21 @@ import com.dispares.lucatinder.model.Usuario;
 	@Repository
 	@Transactional(readOnly = true)
 	public class DaoUsuarioImpl implements DaoUsuarioCustom {
+		
+		private static final Logger logger = LoggerFactory.getLogger(DaoUsuarioImpl.class);
 
 		@PersistenceContext 
 		EntityManager entityManager;
-		 
-		 //Para entender un ejemplo de como se pueden hacer funciones personalizadas
+		
+		/**Metodo que devuelve una lista de 20 usuarios aleatorios
+		 * @author alvaro
+		 * @return lista de usuarios
+		*/
 		@Override
 		public List<Usuario> getBusquedaSimple() {
+			logger.info("Entro en el metodo getBusquedaSimple");
 			Query query = entityManager.createNativeQuery("SELECT * FROM lucatinder.usuarios ORDER BY RAND() LIMIT 20;");
+			logger.info("Salgo del metodo getBusquedaSimple");
 	        return query.getResultList();
 		}
 
