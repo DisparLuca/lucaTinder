d72c33efb03b6203e937dc7dcf334e1026bc0d15
PMGely <pablo.gely.91@gmail.com>
2020-02-27 12:42:12 +0100

    1.6.1_crear_controlador_de_usuario_Pablo
    Se crea la estructura del controlador de usuarios

---
+++ LucaTinder/src/main/java/com/dispares/lucatinder/control/ControladorUsuarios.java
@@ -0,0 +1,13 @@
+package com.dispares.lucatinder.control;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Controlador de usuarios con anotaciones de Spring
+ * @author Equipo3 LucaTinder
+ */
+@Controller
+public class ControladorUsuarios {
+	
+	
+}
