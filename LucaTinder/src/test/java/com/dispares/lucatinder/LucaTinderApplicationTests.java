c3ca21e00511f57586339781dacd263c31cc18b9
Davidubidu <davidfercios@gmail.com>
2020-02-28 12:51:24 +0100

    creado en package com.dispares.lucatinder el test usuarioCreado
    en el test se crea un usuario y se testea que existe y es igual en la
    BBDD. Se usan los métodos recuperarUsuario y borrarUsuario, que aún no
    están creados.

--- LucaTinder/src/test/java/com/dispares/lucatinder/LucaTinderApplicationTests.java
+++ LucaTinder/src/test/java/com/dispares/lucatinder/LucaTinderApplicationTests.java
@@ -2,6 +2,9 @@ package com.dispares.lucatinder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 import com.dispares.lucatinder.control.ControladorUsuarios;
+import com.dispares.lucatinder.model.Usuario;
+import com.dispares.lucatinder.service.Servicios;
 
 
 
@@ -26,7 +31,8 @@ class LucaTinderApplicationTests {
 
     @Autowired
     private ControladorUsuarios  control;
-    
+    @Autowired
+    private Servicios servicio;
     
     /**	
 	 * test para comprobar el contexto ( true is true)
@@ -46,6 +52,20 @@ class LucaTinderApplicationTests {
 	@Test
 	void contextLoads1() throws Exception{
 		assertThat(control).isNotNull();
+		
 	}
-
+	
+	@Test
+	void usuarioCreado() throws Exception{
+		
+		ArrayList<String> categorias = new ArrayList<String>();
+		categorias.add("test1");
+		categorias.add("test1");
+		Usuario usuario = new Usuario("longevina", 100, 1, "Ciudadlongeva", categorias, "fotovintage");
+		servicio.salvarUsuario(usuario);
+		servicio.recuperarUsuario(usuario.getNombre());
+		assertThat(servicio.recuperarUsuario(usuario.getNombre())).isEqualTo(usuario);
+		servicio.borrarUsuario();
+	}
+	
 }
