b032e613d44442beb415ad598b7fcbb74fa25114
jbermejoi <jbermejoi@alumnos.unex.es>
2020-02-28 13:45:16 +0100

    borrado clase repetida comnetario en service ImpleServicios salvarUsuario

--- LucaTinder/src/main/java/com/dispares/lucatinder/dao/CustomDaoUsuario.java
+++
@@ -1,5 +0,0 @@
-package com.dispares.lucatinder.dao;
-
-public interface CustomDaoUsuario {
-
-}
--- LucaTinder/src/main/java/com/dispares/lucatinder/dao/ImpleUsuario.java
+++
@@ -1,8 +0,0 @@
-package com.dispares.lucatinder.dao;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ImpleUsuario implements CustomDaoUsuario {
-
-}
