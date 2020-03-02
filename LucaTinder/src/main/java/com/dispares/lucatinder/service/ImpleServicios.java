6a4ed6e109b44d33f64567a284ec8ae2d3e116ee
PMGely <pablo.gely.91@gmail.com>
2020-03-02 14:48:29 +0100

    6.2_Implementar_servicio_Getususario_Pablo
    Sin terminar, necesito Dao

--- LucaTinder/src/main/java/com/dispares/lucatinder/service/ImpleServicios.java
+++ LucaTinder/src/main/java/com/dispares/lucatinder/service/ImpleServicios.java
@@ -1,5 +1,7 @@
 package com.dispares.lucatinder.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,16 @@ public class ImpleServicios implements Servicios{
 	public void salvarUsuario(Usuario usuario) {
 		// TODO Auto-generated method stub
 		///usuario.save(usuario);
+	}
+	
+	/**Metodo que devuelve un Usuario,si lo encuentra, cuando se le proporciona un id
+	 * @author pablo
+	 * @param id del usuario 
+	 * @return usuario del id proporcionado
+	*/
+	@Override
+	public Optional<Usuario> getUsuario(int id) {
+		return usuarioDAO.findById(id);
 	}
 
 }
--- LucaTinder/src/main/java/com/dispares/lucatinder/service/Servicios.java
+++ LucaTinder/src/main/java/com/dispares/lucatinder/service/Servicios.java
@@ -1,5 +1,7 @@
 package com.dispares.lucatinder.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.dispares.lucatinder.model.Usuario;
@@ -14,4 +16,6 @@ import com.dispares.lucatinder.model.Usuario;
 @Service
 public interface Servicios {
 	public void salvarUsuario(Usuario usuario);
+
+	Optional<Usuario> getUsuario(int id);
 }
