853795891cdd9ad344cf2395785368e00dadb769
PMGely <pablo.gely.91@gmail.com>
2020-03-04 14:34:33 +0100

    Comunicacion

--- LucaTinder/src/main/java/com/dispares/lucatinder/control/ControladorUsuarios.java
+++ LucaTinder/src/main/java/com/dispares/lucatinder/control/ControladorUsuarios.java
@@ -1,8 +1,10 @@
 package com.dispares.lucatinder.control;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.validation.Valid;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -150,9 +153,11 @@ public class ControladorUsuarios {
 		return servUsuario.getUsuario(1).get();	
 	}
 	 
+	 @CrossOrigin(origins = "http://localhost:4200")
 	 @RequestMapping(value = "/leerListaUsuariosRest", method = RequestMethod.GET)
-	public Stream<Usuario> leerListaUsuariosRest() {
-		return servUsuario.getLikeados(1).stream();
+	public Collection<Usuario> leerListaUsuariosRest() {
+		 System.out.println(servUsuario.getLikeados(1).stream().collect(Collectors.toList()));
+		return servUsuario.getLikeados(1).stream().collect(Collectors.toList());
 	}
 	 
 		/**	
